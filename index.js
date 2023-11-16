ee789cf50de9bc3528addfe2b4fec44f1befde8f
mdriyad45 <jakariyakabirriyad@gmail.com>
2023-11-17 00:25:33 +0600

    fix display quizeContainer

--- index.js
+++ index.js
@@ -46,17 +46,19 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
+  console.log(data)
   quizData = data;
-  displayQuiz(data);
+  displayQuiz(quizData);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
-  if (!data) {
+  // if (!data) {
+  //   //return;
+  // }
+    console.log(data);
     quizContainer.innerHTML = "";
-    return;
-  }
 
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
@@ -64,7 +66,7 @@ const displayQuiz = (data) => {
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
